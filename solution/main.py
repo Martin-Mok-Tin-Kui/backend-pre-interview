6efe59a915a0e5291df862868a4171316fb2fa26
tkmok <hkusttkmok@gmail.com>
2020-12-23 21:34:35 +0800

    solve all the test cases and write to file

--- solution/main.py
+++ solution/main.py
@@ -1,19 +1,39 @@
 from solver import Solver
 
-s = Solver()
+with open('../sudoku.txt', 'r') as f:
+    # init the solver
+    s = Solver()
 
-testboard = [
-  [ 8, 0, 0, 0, 0, 0, 0, 0, 0 ],
-  [ 0, 0, 3, 6, 0, 0, 0, 0, 0 ],
-  [ 0, 7, 0, 0, 9, 0, 2, 0, 0 ],
-  [ 0, 5, 0, 0, 0, 7, 0, 0, 0 ],
-  [ 0, 0, 0, 0, 4, 5, 7, 0, 0 ],
-  [ 0, 0, 0, 1, 0, 0, 0, 3, 0 ],
-  [ 0, 0, 1, 0, 0, 0, 0, 6, 8 ],
-  [ 0, 0, 8, 5, 0, 0, 0, 1, 0 ],
-  [ 0, 9, 0, 0, 0, 0, 4, 0, 0 ] 
-]
+    board = []
+    grid_name = ""
+    
+    # create and open file to write solutions to
+    f2 = open('../solutions.txt', 'w') 
 
-s.solve(testboard)
+    for line in f:
+        line = line.strip()
+        if "Grid" in line:
+            grid_name = line
+            count = 0
+        else:
+            board.append([int(char) for char in line])
+            count += 1
+            # A board was fully read
+            if count == 9:
+                s.solve(board)
 
-print("result:",testboard)
+                # write result to file
+                f2.write(grid_name + "\n")
+                # write board result row by row
+                for row in board:
+                    row = "".join([str(x) for x in row])
+                    f2.write(row+"\n")
+                
+                # Find the sum of the first three numbers in the top row (from the left)
+                f2.write(f"sum: {str(sum(board[0][:3]))}\n")
+
+                # finish proccessing the whole board, clear the board for next test case
+                board = []
+
+    # close the writing file once all solutions found
+    f2.close()
